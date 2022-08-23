91cf23ac4f43eb1e57f00815f6de36028a7c390d
Fabricio Silva <F.sillva2507@gmail.com>
2022-08-23 20:07:25 -0300

    criando rotas

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
-import {Link} from "react-router-dom"
+import {useParams} from "react-router-dom"
 
 export function User() {
+    
+
+
     return (
-        <div>User
-            <Link to='/'>Home</Link>
-        </div>
+        <div>User</div>
 
     )
 }
